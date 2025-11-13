3c5f27d45947677e19e08e776be20b364e6f6010
OscarArantegui <aranteguijimenez24@iespabloserrano.com>
2025-11-13 08:41:04 +0100

    arreglamos error

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,9 +1,9 @@
 public class Calculadora {
     public int sumar(int a, int b) {
-        return a + 2*b;
+        return a + b;
     }
 
     public int restar(int a, int b) {
-        return a - 2*b;
+        return a - b;
     }
 }
